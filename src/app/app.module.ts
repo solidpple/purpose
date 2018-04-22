0665aaefb7294925ebeac0de5cce2e632b612d95
Lee <solidpple@gmail.com>
2018-04-22 20:23:28 +0900

    set PrimeNG Dev Env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// PrimeNG
+import { InputTextModule }  from 'primeng/inputtext';
+import { ButtonModule }  from 'primeng/button';
+import { TableModule }  from 'primeng/table';
+import { DialogModule }  from 'primeng/dialog';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    DialogModule,
+    ButtonModule
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
